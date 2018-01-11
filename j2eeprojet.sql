af8cdc9d2fc78025d51a847bea8dd5e93b1c0761
mmmarzouki <31728512+mmmarzouki@users.noreply.github.com>
2018-01-11 12:00:44 +0100

    sql code database

---
+++ j2eeprojet.sql
@@ -0,0 +1,158 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 11, 2018 at 10:59 AM
+-- Server version: 5.7.19
+-- PHP Version: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `j2eeprojet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categorie`
+--
+
+DROP TABLE IF EXISTS `categorie`;
+CREATE TABLE IF NOT EXISTS `categorie` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categorie`
+--
+
+INSERT INTO `categorie` (`id`, `nom`) VALUES
+(1, 'sport'),
+(2, 'voiture'),
+(3, 'lego'),
+(4, 'peluche');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `commandes`
+--
+
+DROP TABLE IF EXISTS `commandes`;
+CREATE TABLE IF NOT EXISTS `commandes` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `date` varchar(255) NOT NULL,
+  `utilisateur_id` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `utilisateur_id` (`utilisateur_id`),
+  KEY `utilisateur_id_2` (`utilisateur_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `commandes_produits`
+--
+
+DROP TABLE IF EXISTS `commandes_produits`;
+CREATE TABLE IF NOT EXISTS `commandes_produits` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `commande_id` int(10) UNSIGNED NOT NULL,
+  `produit_id` int(10) NOT NULL,
+  `quantite` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `produit_id` (`produit_id`),
+  KEY `categorie constraint` (`commande_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produits`
+--
+
+DROP TABLE IF EXISTS `produits`;
+CREATE TABLE IF NOT EXISTS `produits` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `prix` float NOT NULL,
+  `categorie_id` int(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKrajacxk0shaocjomehg40ap74` (`categorie_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `produits`
+--
+
+INSERT INTO `produits` (`id`, `libelle`, `description`, `prix`, `categorie_id`) VALUES
+(1, 'gaucho', 'ye8leb l jou3', 1, 1),
+(2, 'simba', 'peluche simba', 40, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `utilisateurs`
+--
+
+DROP TABLE IF EXISTS `utilisateurs`;
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `passwd` varchar(255) NOT NULL,
+  `admin` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `nom`, `prenom`, `email`, `passwd`, `admin`) VALUES
+(1, 'Med Melek', 'Marzouki', 'mmm@gmail.com', 'passwd', 0),
+(2, 'Halim', 'Helali', 'hhh@gmail.com', 'passwd', 1);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `commandes`
+--
+ALTER TABLE `commandes`
+  ADD CONSTRAINT `utilisateur constraint` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateurs` (`id`);
+
+--
+-- Constraints for table `commandes_produits`
+--
+ALTER TABLE `commandes_produits`
+  ADD CONSTRAINT `categorie constraint` FOREIGN KEY (`commande_id`) REFERENCES `categorie` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `produit constraint` FOREIGN KEY (`produit_id`) REFERENCES `produits` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `produits`
+--
+ALTER TABLE `produits`
+  ADD CONSTRAINT `FKrajacxk0shaocjomehg40ap74` FOREIGN KEY (`categorie_id`) REFERENCES `categorie` (`id`),
+  ADD CONSTRAINT `produits_ibfk_1` FOREIGN KEY (`categorie_id`) REFERENCES `categorie` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
